87e2f271eb515a3e0289256b3a04c0b5806ecc46
madison kanna <madisonkanna@gmail.com>
2017-01-26 10:31:51 -0800

    Expand canvas

--- js/engine.js
+++ js/engine.js
@@ -25,8 +25,8 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    canvas.width = 505;
-    canvas.height = 606;
+    canvas.width = 1000;
+    canvas.height = 806;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
@@ -117,10 +117,14 @@ var Engine = (function(global) {
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
+                'images/grass-block.png',    // Row 2 of 2 of grass
+                'images/grass-block.png',   // Row 1 of 2 of grass
+                'images/grass-block.png',    // Row 2 of 2 of grass
+                'images/grass-block.png',   // Row 1 of 2 of grass
                 'images/grass-block.png'    // Row 2 of 2 of grass
             ],
-            numRows = 6,
-            numCols = 5,
+            numRows = 8,
+            numCols = 9,
             row, col;
 
         /* Loop through the number of rows and columns we've defined above
